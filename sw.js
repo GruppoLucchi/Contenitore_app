37c7cf5d39fc63b9b8d0e0a6cf4f6acd23cc2449
GruppoLucchi <stefano.santucci@lucchi.com>
2026-08-03 12:44:38 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // service (POST verso lucchi.com) vanno sempre in rete e non sono mai
 // gestite dalla cache.
 
-const CACHE_NAME = "primanota-cassa-v8";
+const CACHE_NAME = "primanota-cassa-v10";
 const APP_SHELL = [
   "./",
   "./index.html",
