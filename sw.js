c64048f339e186da163f05eda4e2d0923e045f93
GruppoLucchi <stefano.santucci@lucchi.com>
2026-09-18 17:42:51 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 // controllerchange nell'index - l'app si aggiorna DA SOLA alla riapertura,
 // senza reinstallare e senza manovre da parte dell'utente.
 
-const CACHE_NAME = "primanota-cassa-v22";
+const CACHE_NAME = "primanota-cassa-v23";
 const APP_SHELL = [
   "./",
   "./index.html",
